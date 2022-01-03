fb97f30d05f6b58f2d724645d419d3d62d0c6d54
519389812 <519389812@qq.com>
2022-01-03 17:11:59 +0800

    update

--- rule/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class RuleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'rule'
-    verbose_name = '绩效规则'
-    verbose_name_plural = '绩效规则'
--- sale/admin.py
+++ sale/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from sale.models import SaleRule
 
-# Register your models here.
+
+class SaleRuleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'require', 'calculation')
+    search_fields = ('name',)
+
+    def get_form(self, request, obj=None, **kwargs):
+        help_texts = {
+            'require': '应由比较符号和数值组成，如“>=5000”，若未按规范设置，则不生效',
+            'calculation': '应由运算符号和数值组成，如“*2”，若未按规范设置，则不生效',
+        }
+        kwargs.update({'help_texts': help_texts})
+        return super(SaleRuleAdmin, self).get_form(request, obj, **kwargs)
+
+
+admin.site.register(SaleRule, SaleRuleAdmin)
