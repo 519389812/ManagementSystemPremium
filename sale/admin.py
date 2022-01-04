3fc2da1006d966db5d0df0a121041768fe67b2d1
519389812 <519389812@qq.com>
2022-01-04 17:18:12 +0800

    update

--- sale/admin.py
+++ sale/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from sale.models import SaleRule
+from sale.models import SalesRule
+from django.db.models import Count, Sum, DateTimeField, DateField, Min, Max, Avg, F, ExpressionWrapper, fields, Value, Func
 
 
-class SaleRuleAdmin(admin.ModelAdmin):
+class SalesRuleAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'require', 'calculation')
     search_fields = ('name',)
 
@@ -12,7 +13,43 @@ class SaleRuleAdmin(admin.ModelAdmin):
             'calculation': '应由运算符号和数值组成，如“*2”，若未按规范设置，则不生效',
         }
         kwargs.update({'help_texts': help_texts})
-        return super(SaleRuleAdmin, self).get_form(request, obj, **kwargs)
+        return super(SalesRuleAdmin, self).get_form(request, obj, **kwargs)
 
 
-admin.site.register(SaleRule, SaleRuleAdmin)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+class SalesRecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'product', 'flight_number', 'passenger', 'ticket', 'emd', 'destination', 'issue_user', 'amount', 'miles', 'user')
+    search_fields = ('product__name', 'flight_number', 'passenger', 'ticket', 'emd', 'destination', 'user__full_name', 'issue_user__full_name')
+    list_filter = ('date', 'user__team')
+
+
+class SalesSummaryAdmin(admin.ModelAdmin):
+    change_list_template = "admin/sales_summary_change_list.html"
+
+    search_fields = ('product__name', 'flight_number', 'passenger', 'ticket', 'emd', 'destination', 'user__full_name', 'issue_user__full_name')
+    list_filter = ('date', 'user__team', 'create_datetime', 'position__name', 'verifier')
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context=extra_context)
+        try:
+            qs = response.context_data['cl'].queryset
+        except (AttributeError, KeyError):
+            return response
+        metrics = {
+            'count': Count('user'),
+            'amount': Sum('amount'),
+            'miles': Sum('miles'),
+            'total': Sum('amount') + Sum('score')/20,
+        }
+        data = list(qs.filter(verify=True).values('user__team__name', 'user__full_name').annotate(**metrics).order_by('-score'))
+        response.context_data['summary'] = data
+        return response
+
+
+admin.site.register(SalesRule, SalesRuleAdmin)
+admin.site.register(SalesRule, SalesRuleAdmin)
+admin.site.register(SalesRule, SalesRuleAdmin)
--- sale/models.py
+++ sale/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from user.models import CustomUser
 
 
-class SaleRule(models.Model):
+class SalesRule(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, verbose_name='销量规则名称')
     require = models.CharField(max_length=100, verbose_name='要求量')
@@ -13,3 +14,45 @@ class SaleRule(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, verbose_name='产品名称')
+
+    class Meta:
+        verbose_name = '产品设置'
+        verbose_name_plural = '产品设置'
+
+    def __str__(self):
+        return self.name
+
+
+class SalesRecord(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateField(verbose_name='日期')
+    product = models.ForeignKey(Product, related_name='salesRecord_product', on_delete=models.CASCADE, verbose_name='产品名称')
+    flight_number = models.CharField(max_length=30, verbose_name='航班号')
+    passenger = models.CharField(max_length=30, verbose_name='旅客姓名')
+    ticket = models.CharField(max_length=30, verbose_name='客票号')
+    emd = models.CharField(max_length=30, verbose_name='emd号')
+    destination = models.CharField(max_length=30, verbose_name='目的地')
+    issue_user = models.ForeignKey(CustomUser, related_name='salesRecord_issue_user', on_delete=models.CASCADE, verbose_name='开票人')
+    amount = models.FloatField(verbose_name='销售金额')
+    miles = models.FloatField(verbose_name='销售里程')
+    user = models.ForeignKey(CustomUser, null=True, blank=True, related_name='salesRecord_user', on_delete=models.CASCADE, verbose_name='经办人')
+
+    class Meta:
+        verbose_name = '销售记录'
+        verbose_name_plural = '产品记录'
+
+    def __str__(self):
+        return self.id
+
+
+class SalesSummary(SalesRecord):
+
+    class Meta:
+        proxy = True
+        verbose_name = '销售统计'
+        verbose_name_plural = '销售统计'
